d6b5477dc13b93d196a52ae9e40a8c7ea0ccfdeb
ValMass <valerio.massimi@spsistemi.net>
2020-12-11 14:52:32 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
- // apiUrl: 'https://timesheet.api.spsistemi.net/www/timesheetApi/spstimesheet/',
-  apiUrl: 'https://192.168.1.152:30443/spstimesheet/',
+  apiUrl: 'https://timesheet.api.spsistemi.net/www/timesheetApi/spstimesheet/',
+  //apiUrl: 'https://192.168.1.152:30443/spstimesheet/',
   openRouteServiceDirectionEndpoint: 'https://api.openrouteservice.org/v2/directions/driving-car?',
   openRouteServiceGeoEndpoint: 'https://api.openrouteservice.org/geocode/search?',
   openRouteServiceKey: '5b3ce3597851110001cf6248299e335f9c49402b9e470a50a82662ba',
