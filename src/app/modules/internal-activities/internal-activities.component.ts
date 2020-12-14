f6c370730bd98e571f9b15ba0fb0da64c8c2943e
ValMass <valerio.massimi@spsistemi.net>
2020-12-14 10:44:48 +0100

    internal act fix

--- src/app/modules/internal-activities/internal-activities.component.ts
+++ src/app/modules/internal-activities/internal-activities.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InternalActivities } from './model/internal-activities';
+import { InternalActivitiesService } from './services/internal-activities.service';
 
 @Component({
   selector: 'app-internal-activities',
@@ -8,9 +9,23 @@ import { InternalActivities } from './model/internal-activities';
 })
 export class InternalActivitiesComponent implements OnInit {
   InternalActivitiesList :InternalActivities[];
-  constructor() { }
+  constructor(
+    private internalActivities: InternalActivitiesService
+    ) { }
 
   ngOnInit(): void {
+    let data1 = new Date();
+    let data2 = new Date();
+    this.internalActivities.createInternalActivities("1", "ciccio", data1, data2, 1).subscribe(
+      res => {
+        console.log(res);
+      }
+  );
+    this.internalActivities.getInternalActivitiesList("1").subscribe(
+        res => {
+          console.log(res);
+        }
+    );
   }
 
 }
--- src/app/modules/internal-activities/services/internal-activities.service.ts
+++ src/app/modules/internal-activities/services/internal-activities.service.ts
@@ -1,3 +1,4 @@
+import { UserAdminCreationComponent } from './../../user-admin/user-admin-creation/user-admin-creation.component';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
@@ -11,8 +12,13 @@ export class InternalActivitiesService {
     private http: HttpClient
     ) { }
 
+  createInternalActivities( officesid, name, startdate, enddate, referente){
+    const url = environment.apiUrl + 'internalActivities/createInternalActivities.php';
+    return this.http.post(url, {officesid, name, startdate, enddate, referente});
+
+  }
   getInternalActivitiesList(id){
-    const url = environment.apiUrl + '/offices/createOffices.php';
+    const url = environment.apiUrl + '/internalActivities/listInternalActivitiesAndOffice.php';
     return this.http.post(url, id);
 
   }
