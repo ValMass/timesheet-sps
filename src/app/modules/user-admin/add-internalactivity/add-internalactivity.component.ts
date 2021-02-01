7abd0d334700010823556a003da8059cf1c8f623
christianGasba <gasbarronechristian@gmail.com>
2021-02-01 16:27:44 +0100

    update add internalactivity

--- src/app/modules/user-admin/add-internalactivity/add-internalactivity.component.ts
+++ src/app/modules/user-admin/add-internalactivity/add-internalactivity.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, Inject } from '@angular/core';
@@ -5,29 +6,58 @@ import { Component, OnInit, Inject } from '@angular/core';
 @Component({
   selector: 'app-add-internalactivity',
   templateUrl: './add-internalactivity.component.html',
-  styleUrls: ['./add-internalactivity.component.css']
+  styleUrls: ['./add-internalactivity.component.css'],
+  providers: [DatePipe]
 })
 export class AddInternalactivityComponent implements OnInit {
   public internalActivitiesList : any[] = [];
+  public internalForm: FormGroup;
   public submitted = false;
   public internalAct: any = null;
+  public sede : string;
+  public startdate : string;
+  public enddate : string;
+  
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef <AddInternalactivityComponent>,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    public datepipe: DatePipe,
   ) { }
 
   ngOnInit(): void {
+    this.internalForm = this.formBuilder.group({
+      ruolo:       ['',[Validators.required]],
+      internalAct: ['',[Validators.required]]
+    })
     this.internalActivitiesList = this.data.internalActivitiesList;
+    this.sede = "Nessuna attività interna selezionata";
+    this.startdate = "Nessuna attività interna selezionata";
+    this.enddate = "Nessuna attività interna selezionata";
   }
 
-  submit(f){
-    this.dialogRef.close({ data: f.value });
+  submit(){
+    this.submitted = true;
+    if(!this.internalForm.invalid){
+      this.dialogRef.close({ data: this.internalForm.value });
+    }
   }
 
   close() {
     this.dialogRef.close();
+  }
+
+  setSedeAndTime(evento){
+    if(evento){
+      this.sede = evento.offi.address;
+      this.startdate = this.datepipe.transform(evento.inat.startdate , 'dd-MM-yyyy');
+      this.enddate = this.datepipe.transform(evento.inat.enddate , 'dd-MM-yyyy');
+    }else{
+      this.sede = "Nessuna attività interna selezionata";
+      this.startdate = "Nessuna attività interna selezionata";
+      this.enddate = "Nessuna attività interna selezionata";
+    }
   }
 
 }
