fc94db034a6df356820f6df784b4072be901c655
christianGasba <gasbarronechristian@gmail.com>
2021-02-23 11:46:37 +0100

    wip user-admin-new-password

--- src/app/modules/user-admin/user-admin-new-password/user-admin-new-password.component.ts
+++ src/app/modules/user-admin/user-admin-new-password/user-admin-new-password.component.ts
@@ -10,10 +10,13 @@ import { Component, Inject, OnInit } from '@angular/core';
 export class UserAdminNewPasswordComponent implements OnInit {
 
   //password
-  psw: string = "password";
-  checkPsw : string = "password";
+  pswType: string = "password";
+  checkPswType : string = "password";
+  oldPswType : string = "password";
   id : string = '';
   submited : boolean = false;
+  isUser : boolean = false;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef <UserAdminNewPasswordComponent>,
@@ -26,19 +29,27 @@ export class UserAdminNewPasswordComponent implements OnInit {
 
   //mostro o nascondo la password a seconda dei casi
   pswHideShow() {
-    if (this.psw === "password") {
-      this.psw = "text";
+    if (this.pswType === "password") {
+      this.pswType = "text";
     } else {
-      this.psw = "password";
+      this.pswType = "password";
     }
   }
 
   //mostro o nascondo la password a seconda dei casi
   checkPswHideShow() {
-    if (this.checkPsw === "password") {
-      this.checkPsw = "text";
+    if (this.checkPswType === "password") {
+      this.checkPswType = "text";
     } else {
-      this.checkPsw = "password";
+      this.checkPswType = "password";
+    }
+  }
+
+  oldPswHideShow(){
+    if (this.oldPswType === "password") {
+      this.oldPswType = "text";
+    } else {
+      this.oldPswType = "password";
     }
   }
 
@@ -46,7 +57,11 @@ export class UserAdminNewPasswordComponent implements OnInit {
     this.submited = true
     if(f.value.password === f.value.checkPassword){
       if(f.value.password.length >= 6){
-        this.dialogRef.close({ data: {userId : this.id , newPassword : f.value.password} });
+        if(this.isUser === false){
+          this.dialogRef.close({ data: {userId : this.id , newPassword : f.value.password} });
+        }else{
+          this.dialogRef.close({ data: {userId : this.id , newPassword : f.value.password , oldPassword: f.value.oldpassword} });
+        }
       }
     }else{
       this.toastrService.error('le password non combaciano');
